3e27aab153f78a399f8202ff09ee77dc7be01df8
Noonland1988 <86056308+Noonland1988@users.noreply.github.com>
2021-12-07 06:54:23 +0900

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by 嶋田省吾 on 2021/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
